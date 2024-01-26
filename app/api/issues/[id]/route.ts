19cb5db5c64a06529148fedb4cef956971c14f14
Romeo di Gregorio <romeo_digregorio@hotmail.com>
2024-01-26 09:14:03 +0100

    Build api Delete and Get

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,4 @@
 import { issueSchema } from "@/app/validationSchemas";
-import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
@@ -29,4 +28,23 @@ export async function PATCH(
     })
 
     return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const issue = await prisma?.issue.findUnique({
+        where: { id: parseInt(params.id) }
+    });
+
+    if (!issue) {
+        return NextResponse.json({ error: 'Invalid issue' }, { status: 404 })
+    }
+
+    await prisma?.issue.delete({
+        where: { id: issue.id }
+    });
+
+    return NextResponse.json({});
 }
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -14,3 +14,9 @@ export async function POST(request: NextRequest) {
 
   return NextResponse.json(newIssue, { status: 201 });
 }
+
+export async function GET(request: NextRequest) {
+  const issues = await prisma.issue.findMany();
+
+  return NextResponse.json(issues)
+}
