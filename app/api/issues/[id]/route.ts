64d93021cd4843b6689ddb18c8e8297189de3ef3
Romeo di Gregorio <romeo_digregorio@hotmail.com>
2024-02-06 01:07:57 +0100

    Secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,10 +1,17 @@
 import { issueSchema } from "@/app/validationSchemas";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import authOptions from "../../auth/authOptions";
 
 export async function PATCH(
     request: NextRequest,
     { params }: { params: { id: string } }
 ) {
+
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({}, { status: 401 })
+
 
     const body = await request.json();
     const validation = issueSchema.safeParse(body);
@@ -34,6 +41,12 @@ export async function DELETE(
     request: NextRequest,
     { params }: { params: { id: string } }
 ) {
+
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({}, { status: 401 })
+
+
     const issue = await prisma?.issue.findUnique({
         where: { id: parseInt(params.id) }
     });
