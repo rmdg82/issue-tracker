5c75ba6a0863de958dceeb0a2344a4adcd746c62
Romeo di Gregorio <romeo_digregorio@hotmail.com>
2024-02-17 23:53:10 +0100

    Add Metatada

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,6 +7,7 @@ import DeleteIssueButton from './DeleteIssueButton';
 import { getServerSession } from 'next-auth';
 import authOptions from '@/app/api/auth/authOptions';
 import AssigneeSelect from './AssigneeSelect';
+import { Metadata } from 'next';
 
 interface Props {
   params: { id: string };
@@ -40,5 +41,16 @@ const IssueDetailPage = async ({ params }: Props) => {
     </Grid>
   );
 };
+
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  return {
+    title: issue?.title,
+    description: 'Details of issue' + issue?.id,
+  };
+}
 
 export default IssueDetailPage;
